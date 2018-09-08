1041c4cec84aa4e060750e21ebe921383a0a1ef6
Shivam Agrawal <shivamagrawal@wireless-10-104-39-229.umd.edu>
2018-09-08 12:23:26 -0400

    modified bond strat

--- portfolio_test.py
+++ portfolio_test.py
@@ -1,4 +1,17 @@
-portfolio = {
-    "BOND": { "buy": min-heap "sell": max-heap },
-    ""
+import sys
+
+main_portfolio = {
+    "GOOG": { "buy": { "price": sys.maxint, "quantity": 0 } "sell": { "price": sys.minint, "quantity": 0 } },
+    "MSFT": { "buy": { "price": sys.maxint, "quantity": 0 } "sell": { "price": sys.minint, "quantity": 0 } },
+    "AAPL": { "buy": { "price": sys.maxint, "quantity": 0 } "sell": { "price": sys.minint, "quantity": 0 } },
 }
+
+def portfolio_balance(exchange, portfolio, book):
+	# check for updates
+
+	# preform trade
+
+	# GOOG
+	# if someone's sell order is less than someone elses buy order, then execute
+	if portfolio["GOOG"]["sell"] < portfolio["GOOG"]["buy"]:
+		quantity = min(portfolio["GOOG"]["sell"]["quantity"], portfolio["GOOG"]["sell"]["quantity"])
